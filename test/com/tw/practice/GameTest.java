5f7da628564b06b4f3fd269a82182c8071e98ed5
915448715@qq.com <915448715@qq.com>
2018-01-18 16:14:44 +0800

    add gametest

--- test/com/tw/practice/GameTest.java
+++ test/com/tw/practice/GameTest.java
@@ -29,4 +29,52 @@ public class GameTest {
         Mockito.verify(mockedPrinter, Mockito.times(1)).print("please input a 4 digit number:");
         Mockito.verify(mockedPrinter, Mockito.times(1)).print("you win");
     }
+
+    @Test
+    public void shouldReturn0A0BWhenTotallyWrong() throws Exception {
+        Mockito.when(mockedGenerator.generate()).thenReturn("1234");
+        Mockito.when(mockedReader.read()).thenReturn("5678");
+
+        Game game = new Game(mockedGenerator, mockedPrinter, mockedReader);
+
+        game.start();
+        Mockito.verify(mockedPrinter, Mockito.times(1)).print("please input a 4 digit number:");
+        Mockito.verify(mockedPrinter, Mockito.times(1)).print("you lose");
+    }
+
+    @Test
+    public void shouldReturn4A0BWhenInputSameNumber() throws Exception {
+        Mockito.when(mockedGenerator.generate()).thenReturn("1234");
+        Mockito.when(mockedReader.read()).thenReturn("4321");
+
+        Game game = new Game(mockedGenerator, mockedPrinter, mockedReader);
+
+        game.start();
+        Mockito.verify(mockedPrinter, Mockito.times(1)).print("please input a 4 digit number:");
+        Mockito.verify(mockedPrinter, Mockito.times(1)).print("you lose");
+    }
+
+    @Test
+    public void shouldReturn1A1BWhenInputSameNumber() throws Exception {
+        Mockito.when(mockedGenerator.generate()).thenReturn("1234");
+        Mockito.when(mockedReader.read()).thenReturn("1567");
+
+        Game game = new Game(mockedGenerator, mockedPrinter, mockedReader);
+
+        game.start();
+        Mockito.verify(mockedPrinter, Mockito.times(1)).print("please input a 4 digit number:");
+        Mockito.verify(mockedPrinter, Mockito.times(1)).print("you lose");
+    }
+
+    @Test
+    public void shouldReturn2A1BWhenInputSameNumber() throws Exception {
+        Mockito.when(mockedGenerator.generate()).thenReturn("1234");
+        Mockito.when(mockedReader.read()).thenReturn("1562");
+
+        Game game = new Game(mockedGenerator, mockedPrinter, mockedReader);
+
+        game.start();
+        Mockito.verify(mockedPrinter, Mockito.times(1)).print("please input a 4 digit number:");
+        Mockito.verify(mockedPrinter, Mockito.times(1)).print("you lose");
+    }
 }
